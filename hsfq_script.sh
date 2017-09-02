e5fa203e07f181fa98d39c2b1f9ed246a4e4319a
896660689 <896660689@qq.com>
2017-09-02 14:36:16 +0800

    Update hsfq_script.sh

--- hsfq_script.sh
+++ hsfq_script.sh
@@ -157,8 +157,7 @@ else
 		sed -i '/conf-dir/d' /etc/storage/dnsmasq/dnsmasq.conf
 	else
 		echo -e "\033[41;37m 开始写入启动代码 \e[0m\n"
-	fi
-	echo "listen-address=${route_vlan},127.0.0.1
+		echo "listen-address=${route_vlan},127.0.0.1
 # 添加监听地址
 # 开启日志选项
 log-queries
@@ -170,8 +169,8 @@ log-async=50
 # 指定服务器'域名''地址'文件夹
 conf-dir=/etc/storage/dnsmasq.d/conf
 # conf-file=/etc/storage/dnsmasq.d/conf/hosts_fq.conf" >> /tmp/tmp_dnsmasq.conf
-	cat /tmp/tmp_dnsmasq.conf | sed -E -e "/#/d" >> /etc/storage/dnsmasq/dnsmasq.conf; sleep 3
-	rm /tmp/tmp_dnsmasq.conf
+		cat /tmp/tmp_dnsmasq.conf | sed -E -e "/#/d" >> /etc/storage/dnsmasq/dnsmasq.conf;sleep 3
+		rm /tmp/tmp_dnsmasq.conf
 fi
 
 if [ -f "/etc/storage/post_iptables_script.sh" ]; then
